1ecaf425be12e6527860dfefa681645752c1aee4
zhengxuewen <3444543076@qq.com>
2020-12-15 20:41:24 +0800

    feat:spu页面全部功能完成（2）。

--- src/views/product/spu/spuUpdateList.vue
+++ src/views/product/spu/spuUpdateList.vue
@@ -1,15 +1,15 @@
 <template>
   <el-card style="margin-top: 20px">
-    <!-- 
-    表单校验：
-      1.整体通过form表单中
-      2.通过prop属性来定义表单项名称
-      3.定义表单校验规则
-        - 在data中定义rules
-        - 绑定给form
-      4.校验表单
-        - 给form绑定ref
-        - this.$refs.spuForm.validate 校验表单
+    <!--
+      表单校验：
+        1. 整体form表单中
+        2. 通过prop属性来定义表单项名称
+        3. 定义表单校验规则
+          - 在data中定义rules
+          - 绑定给form
+        4. 校验表单
+          - 给form绑定ref
+          - this.$refs.spuForm.validate 校验表单
      -->
     <el-form label-width="80px" :model="spu" :rules="rules" ref="spuForm">
       <el-form-item label="SPU名称" prop="spuName">
@@ -32,7 +32,7 @@
           v-model="spu.description"
         ></el-input>
       </el-form-item>
-      <el-form-item label="SPU图片">
+      <el-form-item label="SPU图片" prop="imageList">
         <el-upload
           accept="image/*"
           class="avatar-uploader"
@@ -113,15 +113,14 @@
             <template v-slot="{ row, $index }">
               <el-popconfirm
                 @onConfirm="delSpuSaleAttr($index)"
-                :title="`确定删除${row.saleAttrName}吗`"
-              >
-                <el-button
+                :title="`确定删除 ${row.saleAttrName} 吗？`"
+                ><el-button
                   type="danger"
                   icon="el-icon-delete"
                   size="mini"
                   slot="reference"
-                ></el-button>
-              </el-popconfirm>
+                ></el-button
+              ></el-popconfirm>
             </template>
           </el-table-column>
         </el-table>
@@ -156,16 +155,17 @@ export default {
       spuSaleAttrList: [], // 当前SPU销售属性列表
       saleAttrValueText: "",
       rules: {
-        //表单校验规则对象
-        spuName: [{ required: true, message: "请输入SPU品牌" }],
-        tmId: [{ required: true, message: "请选择品牌" }],
-        description: [{ required: true, message: "请输入SPU描述" }],
+        // 表单校验规则对象
+        spuName: [{ required: true, message: "请输入SPU名称~" }],
+        tmId: [{ required: true, message: "请选择品牌~" }],
+        description: [{ required: true, message: "请输入SPU描述~" }],
         imageList: [{ validator: this.imageListValidator, required: true }],
         sale: [{ validator: this.saleValidator, required: true }],
       },
     };
   },
   computed: {
+    // 格式化图片数据
     formatImageList() {
       return this.imageList.map((img) => {
         return {
@@ -177,43 +177,45 @@ export default {
     },
     filterSaleAttrList() {
       return this.saleAttrList.filter((sale) => {
-        // this.spuSaleAttrList.indexOf() // 只适用于数组中是基本类型
-        // 找到了返回{}  没有找到返回undefined
         return !this.spuSaleAttrList.find(
           (spuSale) => spuSale.baseSaleAttrId === sale.id
-        ); // 适用于数组中是引用类型
+        );
       });
     },
   },
   methods: {
     imageListValidator(rule, value, callback) {
       if (this.imageList.length > 0) {
-        //校验通过
+        // 校验通过
         callback();
         return;
       }
-      //校验失败
-      callback(new Error("请上传至少一张图片"));
+      // 校验失败
+      callback(new Error("请上传至少一张图片~"));
     },
     saleValidator(rule, value, callback) {
+      // 判断至少选择一个销售属性
       if (this.spuSaleAttrList.length === 0) {
-        callback(new Error("请至少选择一个销售属性"));
+        callback(new Error("请选择至少一个销售属性~"));
         return;
       }
 
-      //判断销售属性中至少有一个销售属性值
+      // 判断销售属性中至少添加一个销售属性值
       const isNotOk = this.spuSaleAttrList.some(
         (sale) => sale.spuSaleAttrValueList.length === 0
       );
+
       if (isNotOk) {
         callback(new Error("销售属性至少添加一个销售属性值, 请添加~"));
         return;
       }
+
+      callback();
     },
     save() {
       this.$refs.spuForm.validate(async (valid) => {
         if (valid) {
-          console.log("校验通过~");
+          // console.log("校验通过~");
           // 收集数据
           const spu = {
             ...this.spu, // 展开数据
@@ -226,6 +228,9 @@ export default {
           if (result.code === 200) {
             // 切换回showList
             this.$emit("showList", this.spu.category3Id);
+            // this.$nextTick(() => {
+            //   this.$bus.$emit("change", { category3Id: this.spu.category3Id });
+            // });
             this.$message.success("更新SPU成功~");
           } else {
             this.$message.error(result.message);
@@ -233,24 +238,23 @@ export default {
         }
       });
     },
-    //删除整个销售属性
+    // 删除整个销售属性
     delSpuSaleAttr(index) {
-      // console.log(index);
+      console.log(index);
       this.spuSaleAttrList.splice(index, 1);
     },
-    //删除单个销售属性值
+    // 删除单个销售属性值
     delTag(index, row) {
       row.spuSaleAttrValueList.splice(index, 1);
     },
-    //显示编辑输入框
+    // 显示编辑输入框
     edit(row) {
       this.$set(row, "edit", true);
       this.$nextTick(() => {
         this.$refs.input.focus();
       });
     },
-
-    //添加销售属性值
+    // 添加销售属性值
     editCompleted(row) {
       if (this.saleAttrValueText) {
         row.spuSaleAttrValueList.push({
@@ -262,8 +266,10 @@ export default {
         // 添加完成数据清空
         this.saleAttrValueText = "";
       }
+
       row.edit = false;
     },
+
     addSpuSaleAttr() {
       // 选中的销售属性
       const { sale, id } = this.spu;
@@ -279,8 +285,9 @@ export default {
       // 清空选中的销售属性id
       this.spu.sale = "";
     },
-    //上次图片之前触发的回调
+    // 上次图片之前触发的回调
     beforeAvatarUpload(file) {
+      // console.log(file);
       const imgTypes = ["image/jpg", "image/png", "image/jpeg"];
       // 检测文件类型
       const isValidType = imgTypes.indexOf(file.type) > -1;
@@ -297,12 +304,13 @@ export default {
       // 返回值为false，代表不可以上传
       return isValidType && isLt;
     },
+    // 上传图片成功的回调
     handleAvatarSuccess(res, file) {
       this.imageList.push({
         uid: file.uid,
-        imgName: file.name,
-        imgUrl: res.data,
-        spuId: this.spu.id,
+        imgName: file.name, // 文件名称
+        imgUrl: res.data, // 图片地址
+        spuId: this.spu.id, // SPU id
       });
     },
     // 处理删除
